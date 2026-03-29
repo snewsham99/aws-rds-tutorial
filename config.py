7fe8e80db19f12ddcc0f6e167ce1c7f31e98986e
snewsham99 <18213141+snewsham99@users.noreply.github.com>
2026-03-29 11:37:37 -0400

    Another attempt

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ Replace these values with your actual RDS connection details
 """
 
 # RDS Connection Settings
-DB_HOST = 'tutorial-db-2.cgzugo4sgrdz.us-east-1.rds.amazonaws.com'
+DB_HOST = 'tutorial-db-3.cgzugo4sgrdz.us-east-1.rds.amazonaws.com'
 DB_PORT = 3306
 DB_USER = 'admin'
-DB_PASSWORD = '[IhopeTHISworks!!]!'  # ← Change this!
-DB_NAME = 'tutorial_db_2'
+DB_PASSWORD = 'qoy5Cati'  # ← Change this!
+DB_NAME = 'tutorial_db_3'
 
 # Connection Pool Settings
 POOL_SIZE = 5
